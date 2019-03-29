d0d19c3fc551b31af49da2e94bec4d59c4dcf645
Holger Münzer <holger.muenzer@gmail.com>
2019-03-29 09:56:12 +0100

    namespace and composer.json added

--- example.php
+++ example.php
@@ -1,5 +1,8 @@
 <?php
 
+        use \hmuenzer\Recurrence;
+        require_once('./src/Recurrence.php');   //you don't need this, if you use an autoloader
+
         $options = array(
           'dtstart' => '19970902T090000',
           'dtend'   => '19970902T100000',
@@ -7,7 +10,6 @@
           'rrule'   => 'FREQ=WEEKLY;UNTIL=19971007T000000Z;WKST=SU;BYDAY=TU,TH'
           );
 
-        require_once('./Recurrence.php');
         $recurrence = new Recurrence($options);
         $recurrence->format = 'Ymd\THisO';
 
